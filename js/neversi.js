44e3334477f7edbe47fca78e8eca37c81f5a2e1e
Nadim Kobeissi <nadim@nadim.cc>
2012-12-07 23:15:22 +0200

    Basic XMPP hooks

--- js/neversi.js
+++ js/neversi.js
@@ -1,3 +1,14 @@
+// Neversi
+// Copyright Nadim Kobeissi, all rights reserved
+
+// Configuration
+var domain = 'crypto.cat';
+var conference = 'conference.crypto.cat';
+var bosh = 'https://crypto.cat/http-bind';
+
+var myNickname;
+var loginCredentials = [];
+
 var neversi = function() {};
 
 //$(window).load(function() {
@@ -285,7 +296,7 @@ $('.square').click(function() {
 	}
 });
 
-// Login form logic
+// Login form logic for fields and buttons
 $('input[type=text]').click(function() {
 	$(this).select();
 });
@@ -301,6 +312,129 @@ $('#play').mouseout(function() {
 		'color': '#CCC'
 	}, 'fast');
 });
+
+// Display a message in the message area
+function showMessage(message) {
+	$('#message').find('span').fadeOut(function() {
+		$(this).text(message);
+		$(this).fadeIn();
+	});
+}
+
+// Handle submission of login form
+$('#login').submit(function() {
+	// Don't submit if form is already being processed
+	if (($('#play').attr('readonly') === 'readonly')) {
+		return false;
+	}
+	$('#play').mouseover().mouseout();
+	//Check validity of nickname and game ID
+	$('#nickname').val($.trim($('#nickname').val()));
+	if (($('#nickname').val() === '')
+		|| ($('#nickname').val() === 'Your nickname')) {
+		showMessage('Please enter a nickname.');
+		$('#nickname').select();
+	}
+	else if (!$('#nickname').val().match(/^\w{1,16}$/)) {
+		showMessage('Your nickname can only contain alphanumeric characters.');
+		$('#nickname').select();
+	}
+	// If everything is okay, then register a randomly generated throwaway XMPP ID and log in.
+	else {
+		myNickname = Strophe.xmlescape($('#nickname').val().toLowerCase());
+		loginCredentials[0] = randomString(64, 1, 1, 1);
+		loginCredentials[1] = randomString(64, 1, 1, 1);
+		registerXMPPUser(loginCredentials[0], loginCredentials[1]);
+		$('#play').attr('readonly', 'readonly');
+		showMessage('Connecting...');
+	}
+	return false;
+});
+
+// Registers a new user on the XMPP server.
+function registerXMPPUser(username, password) {
+	var registrationConnection = new Strophe.Connection(bosh);
+	registrationConnection.register.connect(domain, function(status) {
+		if (status === Strophe.Status.REGISTER) {
+			registrationConnection.register.fields.username = username;
+			registrationConnection.register.fields.password = password;
+			registrationConnection.register.submit();
+		}
+		else if (status === Strophe.Status.REGISTERED) {
+			registrationConnection.disconnect();
+			delete registrationConnection;
+			login(loginCredentials[0], loginCredentials[1]);
+			return true;
+		}
+		else if (status === Strophe.Status.SBMTFAIL) {
+			showMessage('Connection error.');
+			$('#play').removeAttr('readonly');
+			return false;
+		}
+	});
+}
+
+// Logs into the XMPP server, creating main connection/disconnection handlers.
+function login(username, password) {
+	conn = new Strophe.Connection(bosh);
+	conn.connect(username + '@' + domain, password, function(status) {
+		if (status === Strophe.Status.CONNECTING) {
+			
+		}
+		else if (status === Strophe.Status.CONNFAIL) {
+			showMessage('Connection error.');
+		}
+		else if (status === Strophe.Status.CONNECTED) {
+			showMessage('Connected.');
+			conn.muc.join(
+				'lobby@' + conference, myNickname, 
+				function(message) {
+					if (handleMessage(message)) {
+						return true;
+					}
+				},
+				function (presence) {
+					if (handlePresence(presence)) {
+						return true;
+					}
+				}
+			);
+		}
+		else if ((status === Strophe.Status.DISCONNECTED) || (status === Strophe.Status.AUTHFAIL)) {
+			$('#nickname').val('Your nickname');
+			myNickname = null;
+			loginCredentials = [];
+			conn = null;
+			$('#play').removeAttr('readonly');
+		}
+	});
+}
+
+// Logout function
+function logout() {
+	conn.muc.leave('lobby@' + conference);
+	conn.disconnect();
+}
+
+// Logout on browser close
+$(window).unload(function() {
+	logout();
+});
+
+// Generates a random string of length `size` characters.
+// If `alpha = 1`, random string will contain alpha characters, and so on.
+function randomString(size, alpha, uppercase, numeric) {
+	var keyspace = result = '';
+	if (alpha) { keyspace += 'abcdefghijklmnopqrstuvwxyz' }
+	if (uppercase) { keyspace += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' }
+	if (numeric) { keyspace += '0123456789' }
+	for (var i = 0; i !== size; i++) {
+		result += keyspace[Math.floor(Math.random()*keyspace.length)];
+	}
+	return result;
+}
+
+$('#nickname').select();
 
 neversi.newGame();
 
